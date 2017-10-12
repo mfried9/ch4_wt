450d2c695f6e7ad96047c3a11058b1b52ad714e5
Matthew Fried <mfried@uncc.edu>
2017-10-12 00:39:12 +0000

    Added movie_params to Controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,4 +9,11 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id)
   end
   
+  # add below all other methods
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
 end
